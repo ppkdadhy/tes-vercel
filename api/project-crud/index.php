85cbfdf7a724fc438e98985a192f016fac212065
ppkdadhy <triadhyy@gmail.com>
2026-08-03 00:59:34 +0700

    Fix: Assets routing for Kaiadmin and secure session config

--- api/project-crud/index.php
+++ api/project-crud/index.php
@@ -16,7 +16,7 @@ if (isset($_POST['login'])) { //jika tombol login di tekan
   if ($email == $row['email'] && $pass == $row['password']) {
     $_SESSION['NAME'] = $row['name'];
     // KALAU BERHASIL MASUK KE DASHBOARD
-    header("location:dashboard.php");
+    header("location:create-slider.php");
   } else {
     // KALAU GAGAL TETAP DI LOGIN
     header("location:index.php");
